8c901b23e0e2543de70ec6d3af735248e755e80e
mino <minho0719@a.ut.ac.kr>
2018-11-16 16:18:30 +0900

    첫단어 초기화 기능 구현
    10개 정도의 임의의 단어에서 무작위로 고르는 기능을 구현
    버튼으로 배열된 단어위 위치를 무작위로 섞어 주는 기능을 구현

--- puzzle/js/word.js
+++ puzzle/js/word.js
@@ -1,5 +1,26 @@
+var words = ['apple','banana','car','door','emmit','fake','grooming','hire','icecream'];
 var str = document.getElementById('word1').innerHTML;
+str = initWord();
 var word2 = document.getElementById('word2');
+
+function initWord() {
+    var n = Math.floor(Math.random()*9);
+    console.log(words[n])
+    var word = words[n].split('');
+    console.log(word.length);
+    var shuffle = [];
+    for(var i = 0; i<word.length; i++) {
+        while(true) {            
+            var n2 = Math.floor(Math.random()*word.length);
+            if(word[n2] !== '') {
+                shuffle.push(word[n2]);
+                word[n2] = '';
+                break;
+            }
+        }
+    }
+    return shuffle.join("");
+}
 
 var game = {};
 game.word = str.split('');
